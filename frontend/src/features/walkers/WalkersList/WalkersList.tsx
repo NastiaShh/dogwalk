be2552c6dca355fae84f10e0eac12a4a570af2ef
Ulyana <lizsong691@gmail.com>
2022-12-23 10:29:44 +0300

    Create walkers list

---
+++ frontend/src/features/walkers/WalkersList/WalkersList.tsx
@@ -0,0 +1,12 @@
+import WalkerView from "../WalkerView/WalkerView";
+
+
+function WalkersList(): JSX.Element {
+  return (
+    <div>
+      <WalkerView />
+    </div>
+  )
+}
+
+export default WalkersList
