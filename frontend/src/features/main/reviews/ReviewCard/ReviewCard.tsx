1540dc12316b93e5333860cb57595fc65b6227a5
Daniela <dk96.dk96@gmail.com>
2022-12-26 20:29:09 +0300

    add rating paws

--- frontend/src/features/main/reviews/ReviewCard/ReviewCard.tsx
+++ frontend/src/features/main/reviews/ReviewCard/ReviewCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './ReviewCard.module.css';
 import quotesIcon from '../img/quotes.svg';
 import Review from '../types/Review';
+import { Stack, Rating } from '@mui/material';
+import PetsIcon from '@mui/icons-material/Pets';
 
 type ReviewProps = {
   review: Review;
@@ -15,7 +17,19 @@ export default function ReviewCard({
       <div className={style.head}>
         <div className={style.info}>
           <div className={style.title}>{review.name}</div>
-          <div className={style.rating}></div>
+          <div className={style.rating}>
+            <Stack spacing={2}>
+              <Rating
+                value={review.rating}
+                precision={0.5}
+                size='medium'
+                icon={<PetsIcon fontSize='inherit' />}
+                emptyIcon={<PetsIcon fontSize='inherit' />}
+                name="read-only"
+                readOnly
+              />
+            </Stack>
+          </div>
           <div className={style.quotesIcon}>
             <img className={style.quotesIconSvg} src={quotesIcon} alt={'quotes-icon'} />
           </div>
