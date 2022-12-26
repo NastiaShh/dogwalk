f5d7d494aab6d67465b4e770ea90c905278ec279
Anastasiia Shamrai <sky.blush@ya.ru>
2022-12-26 18:05:10 +0300

    add Users table

---
+++ backend/db/models/user.js
@@ -0,0 +1,29 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate() {
+      // User.Pets = User.hasMany(Pet, { foreignKey: 'user_id' });
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    photo: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
