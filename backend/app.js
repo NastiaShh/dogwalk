1faa48ce9c72b9b7d13b86b9b9315b362db0ddcf
Anastasiia Shamrai <sky.blush@ya.ru>
2022-12-26 15:40:04 +0300

    deploy site

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const expressConfig = require('./config/express');
 
@@ -10,6 +11,10 @@ expressConfig(app);
 
 // подключаем маршрутизацию
 app.use('/api/walkers', walkersRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 // eslint-disable-next-line prefer-destructuring
 const PORT = process.env.PORT;
