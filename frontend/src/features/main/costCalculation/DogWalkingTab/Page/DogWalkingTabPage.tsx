dc8607ba15c9011dcfee593968ed69a08ce8008d
Daniela <dk96.dk96@gmail.com>
2022-12-30 09:55:30 +0300

    add toggle buttons for changing number of dogs

--- frontend/src/features/main/costCalculation/DogWalkingTab/Page/DogWalkingTabPage.tsx
+++ frontend/src/features/main/costCalculation/DogWalkingTab/Page/DogWalkingTabPage.tsx
@@ -1,12 +1,14 @@
 import style from './DogWalkingTabPage.module.css';
 import WalkDurationToggle from "../components/WalkDurationToggle/WalkDurationToggle";
 import RangeSlider from '../components/RangeSlider/RangeSlider';
+import DogsNumberToggle from '../components/DogsNumberToggle/DogsNumberToggle';
 
 export default function DogWalkingTabPage(): JSX.Element {
   return (
     <div className={style.container}>
       <WalkDurationToggle />
       <RangeSlider />
+      <DogsNumberToggle />
     </div>    
   );
 };
---
+++ frontend/src/features/main/costCalculation/DogWalkingTab/components/DogsNumberToggle/DogsNumberToggle.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { toggleButtonClasses } from '@mui/material/ToggleButton';
+import { styled } from '@mui/system';
+
+const MyToggleButton = styled(ToggleButton)(
+  ({ theme }) => `
+    border: none;
+    border-radius: 50% !important;
+    text-transform: none;
+    background-color: inherit;
+    color: #000;
+    font-size: 1.8em;
+    font-weight: bold;
+    padding: 0.01em 0.6em;
+    margin-top: 0.3em;
+
+    &:hover {
+      background-color: inherit;
+      border: 1px solid #398a6a !important;
+    }
+
+    &.${toggleButtonClasses.selected} {
+      background-color: inherit;
+      color: #000;
+      border: 1px solid #398a6a !important;
+    }
+
+    &.${toggleButtonClasses.selected}:hover {
+      background-color: inherit;
+    }
+  `,
+);
+
+const MyToggleButtonGroup = styled(ToggleButtonGroup)(
+  ({ theme }) => `
+  width: 21%;
+  display: flex;
+  justify-content: space-between;
+  font-family: inherit;
+  `,
+);
+
+export default function DogsNumberToggle(): JSX.Element {
+  const [dogsNumber, setDogsNumber] = React.useState(1);
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newDogsNumber: number | null,
+  ): void => {
+    if (newDogsNumber !== null) {
+      setDogsNumber(newDogsNumber);
+    }
+  };
+ 
+  return (
+    <>
+    <div style={{
+      textAlign: 'left',
+      fontSize: '1.2em',
+      marginTop: '2.5em',
+    }}>
+      Количество собак
+    </div>
+    <div>
+      <MyToggleButtonGroup
+        color="primary"
+        value={dogsNumber}
+        exclusive
+        onChange={handleChange}
+        aria-label="Platform"
+      >
+        <MyToggleButton value={1} disableRipple={true}>1</MyToggleButton>
+        <MyToggleButton value={2} disableRipple={true}>2</MyToggleButton>
+        <MyToggleButton value={3} disableRipple={true}>3</MyToggleButton>
+        <MyToggleButton value={4} disableRipple={true}>4</MyToggleButton>
+      </MyToggleButtonGroup>
+    </div>
+    </>
+  )
+}
