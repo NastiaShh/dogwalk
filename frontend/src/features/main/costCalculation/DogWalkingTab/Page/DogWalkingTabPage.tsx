78d71aaab161e4a112288da6354c30933c31ad2a
Daniela <dk96.dk96@gmail.com>
2022-12-31 11:51:59 +0300

    update margins

--- frontend/src/features/main/costCalculation/DogWalkingTab/Page/DogWalkingTabPage.tsx
+++ frontend/src/features/main/costCalculation/DogWalkingTab/Page/DogWalkingTabPage.tsx
@@ -1,16 +1,22 @@
-import style from './DogWalkingTabPage.module.css';
 import WalkDurationToggle from "../components/WalkDurationToggle/WalkDurationToggle";
 import RangeSlider from '../components/RangeSlider/RangeSlider';
 import DogsNumberToggle from '../components/DogsNumberToggle/DogsNumberToggle';
 import CalculatedPrice from '../components/CalculatedPrice/CalculatedPrice';
+import PhoneNameForm from '../components/PhoneNameForm/PhoneNameForm';
 
 export default function DogWalkingTabPage(): JSX.Element {
   return (
-    <div className={style.container}>
+    <div 
+      style={{
+        width: '70%',
+        margin: '2em auto',
+      }}
+    >
       <WalkDurationToggle />
       <RangeSlider />
       <DogsNumberToggle />
       <CalculatedPrice />
+      <PhoneNameForm />
     </div>    
   );
 };
