0f470e13c34d7810163e14b077d9ace1c27c2e40
Daniela <dk96.dk96@gmail.com>
2023-01-07 16:22:42 +0300

    add responsive design

--- frontend/src/features/main/reviews/ReviewsSwiper/ReviewsSwiper.tsx
+++ frontend/src/features/main/reviews/ReviewsSwiper/ReviewsSwiper.tsx
@@ -1,5 +1,3 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -8,11 +6,9 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import { Navigation } from "swiper";
 
 import "./styles.css";
-
-import style from './ReviewsSwiper.module.css'
 
 import { useSelector } from 'react-redux';
 import { selectReviews, selectLoadError, selectLoading } from '../selectors';
@@ -28,6 +24,7 @@ import avatar2 from '../img/avatar2.png';
 import avatar3 from '../img/avatar3.png';
 
 export default function SwiperPage(): JSX.Element {
+  // const reviews = useSelector(selectReviews);
   const loadError = useSelector(selectLoadError);
   const loading = useSelector(selectLoading);
 
@@ -84,33 +81,30 @@ export default function SwiperPage(): JSX.Element {
   ]
   
   return (
-    <div style={{
-      width: '80%',
-      margin: 'auto'
-    }}> 
-        
-      
+    <div className="swiper-container">  
 
       <Swiper
         slidesPerView={1}
-        spaceBetween={10}
+        spaceBetween={0}
         navigation={{
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         }}
-        // pagination={{
-        //   clickable: true,
-        // }}
+        
         breakpoints={{
-          640: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          960: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
           },
-          1024: {
+          1390: {
             slidesPerView: 4,
             spaceBetween: 35,
           },
@@ -118,6 +112,7 @@ export default function SwiperPage(): JSX.Element {
         modules={[Navigation]}
         className="mySwiper"
       >
+        
         <img className='swiper-button-prev' src={prevBtnEnabled} alt='previous' />
         <img className='swiper-button-next' src={nextBtnEnabled} alt='next' />
         
