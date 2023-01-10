1aea07da58d5aef1763ae9c8b40fb5b31fad9038
Anastasiia Shamrai <sky.blush@ya.ru>
2023-01-10 15:22:24 +0300

    fix role

--- frontend/src/features/auth/types/AuthState.ts
+++ frontend/src/features/auth/types/AuthState.ts
@@ -3,9 +3,9 @@ import { User, UserRole } from './UserState';
 type AuthState = {
   authChecked: boolean;
   user?: User;
-  role?: UserRole;
-  emailError?: string,
-  loginError?: string,
+  role: UserRole;
+  // emailError?: string,
+  // loginError?: string,
 };
 
 export default AuthState;
