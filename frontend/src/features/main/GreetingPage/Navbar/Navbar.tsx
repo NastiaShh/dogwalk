8ebc73484a080fcdd14975bb89905c71bda0f215
Anastasiia Shamrai <sky.blush@ya.ru>
2023-01-11 11:08:45 +0300

    update Navbar

--- frontend/src/features/main/GreetingPage/Navbar/Navbar.tsx
+++ frontend/src/features/main/GreetingPage/Navbar/Navbar.tsx
@@ -1,26 +1,36 @@
 import style from '../GreetingPage.module.css';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../../../store';
+import { RootState, useAppDispatch } from '../../../../store';
+import { logout } from '../../../auth/authSlice';
 
 function Navbar(): JSX.Element {
-  const { authChecked, role } = useSelector((state: RootState) => state.auth)
+  const dispatch = useAppDispatch();
+
+  const { authChecked, role } = useSelector((state: RootState) => state.auth);
+
+  const handleLogout = (event: React.FormEvent): void => {
+    event.preventDefault();
+    dispatch(logout());
+  };
+
   return (
     <nav className={style.nav}>
-      {authChecked === true && role === 'admin' ? (
+      {authChecked === true && role === 'admin' && (
         <a href="/" className={style.link}>Заявки</a>
-      ) : (
+      )}
+      {authChecked === true && role === 'user' && (
+        <a href="/" className={style.link}>Личный кабинет</a>
+      )}
+      {authChecked === true && (
+        <a href="/api/auth/logout" className={style.link} onClick={handleLogout}>Выйти</a>
+      )}
+      {authChecked === false && (
         <>
           <a href="#services" className={style.link}>Услуги</a>
           <a href="#cost" className={style.link}>Стоимость</a>
           <a href="#questions" className={style.link}>Вопросы</a>
           <a href="#reviews" className={style.link}>Отзывы</a>
         </>
-      )}
-      {authChecked === true && role === 'user' && (
-        <a href="/" className={style.link}>Личный кабинет</a>
-      )}
-      {authChecked === true && (
-        <a href="/" className={style.link}>Выйти</a>
       )}
     </nav>
   );
