1edd7e44f8aeb94cbab487cec00912496a6e4820
Anastasiia Shamrai <sky.blush@ya.ru>
2023-01-12 14:17:38 +0300

    update Recommendations component

--- frontend/src/features/profile/DogsData/Recommendations/Recommendations.tsx
+++ frontend/src/features/profile/DogsData/Recommendations/Recommendations.tsx
@@ -1,17 +1,23 @@
 import { UseFormRegister } from "react-hook-form";
 import Dog from '../types/Dog'
+import style from './Recommendations.module.css';
 import styles from './../Page/DogPage.module.css';
 
 function Recommendations({ register }: { register: UseFormRegister<Dog> }): JSX.Element {
   return (
     <div>
-      <p>Рекомендации по прогулкам/передержке</p>
-      <textarea {...register("description")} />
+      <p className={style.title}>Рекомендации по прогулкам/передержке</p>
+      <textarea
+        {...register("description")}
+        className={style.description}
+        placeholder="Напишите сюда сведения о собаке (индивидуальные особенности,
+          режим питания, состояние здоровья и другие рекомендации)"
+      />
       <button type="submit" className={styles.buttonNext}>
         готово
       </button>
     </div>
-  )
+  );
 }
 
 export default Recommendations
