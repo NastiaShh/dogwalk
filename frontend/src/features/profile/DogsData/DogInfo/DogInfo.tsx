d5a2932aeb5f01a4d95f1f790d6405c91d359fc6
Anastasiia Shamrai <sky.blush@ya.ru>
2023-01-12 12:54:52 +0300

    update DogInfo component

--- frontend/src/features/profile/DogsData/DogInfo/DogInfo.tsx
+++ frontend/src/features/profile/DogsData/DogInfo/DogInfo.tsx
@@ -1,31 +1,48 @@
 import { UseFormRegister } from "react-hook-form";
 import Dog from '../types/Dog'
-
+import style from './DogInfo.module.css'
 
 function DogInfo({ register }: { register: UseFormRegister<Dog> }): JSX.Element {
   return (
-    <div>
-      <p>Имя собаки</p>
-      <input
-        type="text"
-        {...register("name")}
-      />
-      <p>Порода</p>
-      <input
-        type="text"
-        {...register('breed')}
-      />
-      <p>Возраст</p>
-      <input
-        type="number"
-        {...register('age')}
-      />
-      <p>Размер</p>
-      <select {...register('size')}>
-        <option value="маленькая">Маленькая</option>
-        <option value="средняя">Средняя</option>
-        <option value="большая">Большая</option>
-      </select>
+    <div className={style.container}>
+      <div className={style.parameter}>
+        <label htmlFor="name">Имя собаки</label>
+        <input
+          type="text"
+          placeholder="Шарик"
+          className={style.input}
+          {...register("name")}
+        />
+      </div>
+      <div className={style.parameter}>
+        <label htmlFor="breed">Порода</label>
+        <input
+          type="text"
+          placeholder="Сиба-ину"
+          className={style.input}
+          {...register('breed')}
+        />
+      </div>
+      <div className={style.parameter}>
+        <label htmlFor="age">Возраст</label>
+        <div>
+          <input
+            type="number"
+            placeholder="5"
+            className={`${style.input} ${style.ageInput}`}
+            {...register('age')}
+          />
+          <span className={style.ageSpan}>лет</span>
+        </div>
+      </div>
+      <div className={style.parameter}>
+        <label htmlFor="size">Размер</label>
+        <select {...register('size')} className={style.sizeInput}>
+          <option value="маленькая">Маленькая</option>
+          <option value="средняя" selected>Средняя</option>
+          <option value="большая">Большая</option>
+        </select>
+      </div>
       <button type="button">
         далее
       </button>
@@ -33,4 +50,4 @@ function DogInfo({ register }: { register: UseFormRegister<Dog> }): JSX.Element 
   )
 }
 
-export default DogInfo
+export default DogInfo;
