ff80fdf969dc7fbc95ee172fc2e164e52be0373f
Ulyana <lizsong691@gmail.com>
2023-01-13 14:20:47 +0300

    seeds

--- backend/db/seeders/20221226104906-forms.js
+++ backend/db/seeders/20221226104906-forms.js
@@ -3,62 +3,7 @@ const { ApplicationForm } = require('../models');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up() {
-    await ApplicationForm.bulkCreate([
-      {
-        name: 'Anya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'не обработано',
-      },
-      {
-        name: 'Kolya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'в работе',
-      },
-      {
-        name: 'Tolya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'в работе',
-      },
-      {
-        name: 'Fedya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'не обработано',
-      },
-      {
-        name: 'Fedya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'не обработано',
-      },
-      {
-        name: 'Fedya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'не обработано',
-      },
-      {
-        name: 'Kolya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'завершено',
-      },
-      {
-        name: 'Kostya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'завершено',
-      },
-      {
-        name: 'Anya',
-        phone: '+79999999999',
-        email: 'mail@mail.com',
-        status: 'завершено',
-      },
-    ]);
+    await ApplicationForm.bulkCreate([]);
   },
 
   async down() {
